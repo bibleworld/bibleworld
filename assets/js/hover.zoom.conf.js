fe998399766a3936c6ecf2906e712eec7a4ec6c0
Todd Xu <xntseven@gmail.com>
2016-12-04 00:07:58 -0800

    more

--- assets/js/hover.zoom.conf.js
+++ assets/js/hover.zoom.conf.js
@@ -1,33 +1,38 @@
         $(function() {
-        
-        	
+
+
             $('.blue').hoverZoom({
                 overlayColor: '#3498db',
                 zoom: 0
             });
-            
+
             $('.green').hoverZoom({
                 overlayColor: '#1abc9c',
                 zoom: 0
             });
-            
+
             $('.pink').hoverZoom({
                 overlayColor: '#bd2e75',
                 zoom: 0
             });
-            
+
             $('.black').hoverZoom({
                 overlayColor: '#2f2f2f',
                 zoom: 0
             });
-            
+
             $('.alizarin').hoverZoom({
                 overlayColor: '#e74c3c',
                 zoom: 0
             });
-            
+
+            $('.lightpink').hoverZoom({
+                overlayColor: '#db6e6c',
+                zoom: 0
+            });
+
             /* USAGE
-            
+
             $('#pink').hoverZoom({
                 overlay: true, // false to turn off (default true)
                 overlayColor: '#2e9dbd', // overlay background color
@@ -35,7 +40,7 @@
                 zoom: 25, // amount to zoom (px)
                 speed: 300 // speed of the hover
             });
-            
+
             */
-            
-        }); 
+
+        });
--- assets/js/hover.zoom.js
+++ assets/js/hover.zoom.js
@@ -1,39 +1,39 @@
 (function($){
 
-    $.fn.extend({ 
+    $.fn.extend({
 
         hoverZoom: function(settings) {
- 
+
             var defaults = {
                 overlay: true,
                 overlayColor: '#2e9dbd',
-                overlayOpacity: 0.9,
+                overlayOpacity: 0.5,
                 zoom: 25,
                 speed: 300
             };
-             
+
             var settings = $.extend(defaults, settings);
-         
+
             return this.each(function() {
-            
+
                 var s = settings;
                 var hz = $(this);
                 var image = $('img', hz);
 
                 image.load(function() {
-                    
+
                     if(s.overlay === true) {
                         $(this).parent().append('<div class="zoomOverlay" />');
                         $(this).parent().find('.zoomOverlay').css({
-                            opacity:0, 
-                            display: 'block', 
+                            opacity:0,
+                            display: 'block',
                             backgroundColor: s.overlayColor
-                        }); 
+                        });
                     }
-                
+
                     var width = $(this).width();
                     var height = $(this).height();
-                
+
                     $(this).fadeIn(1000, function() {
                         $(this).parent().css('background-image', 'none');
                         hz.hover(function() {
@@ -60,7 +60,7 @@
                             }
                         });
                     });
-                });    
+                });
             });
         }
     });
